3c4c2dddaba72731256f0ea7246a7fa330f022a1
Mert Kadir <mertkadirarslan@gmail.com>
2022-12-26 19:03:04 +0300

    web-main

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        online: '#3ba55d',
+        idle: '#faa81a',
+        dnd: '#ed4245',
+        offline: '#747f8d'
+      }
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@headlessui/tailwindcss"),
+    require("@headlessui/tailwindcss")({ prefix: "ui" }),
+  ],
+};
